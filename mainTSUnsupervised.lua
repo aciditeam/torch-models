05b0b0bbc8b021f931d68e8a2a75e43ccc9fa33f
tbazin <theis.bazin@ens-cachan.fr>
2016-05-17 18:44:10 +0200

    Cosmetic update

--- mainTSUnsupervised.lua
+++ mainTSUnsupervised.lua
@@ -131,115 +131,124 @@ for k, v in ipairs(modelsList) do
    -- Even for pre-training
    -- TODO
    -- TODO
-   if curModel.pretrain then
-      print('- Performing pretraining.');
-      -- Unsupervised set
-      local unsupData = unSets["TRAIN"];
-      local unsupValid = unSets["VALID"];
-      -- Switch training data to GPU
-      if options.cuda then
-	 unsupData.data = unsupData.data:cuda();
-	 unsupValid.data = unsupValid.data:cuda();
+
+   -- Unsupervised set
+   local unsupData = unSets["TRAIN"];
+   local unsupValid = unSets["VALID"];
+
+   -- Switch training data to GPU
+   if options.cuda then
+      unsupData.data = unsupData.data:cuda();
+      unsupValid.data = unsupValid.data:cuda();
+   end
+
+   -- Set of trained layers
+   trainedLayers = {};
+
+   for l = 1,structure.nLayers do
+      -- Define the pre-training model
+      local model = curModel:definePretraining(structure, l, options);
+      print(tostring(model));
+      -- Activate CUDA on the model
+      if options.cuda then model:cuda(); end
+      -- If classical learning configure the optimizer
+      if (not options.adaptiveLearning) then 
+	 if torch.type(unsupData.data) ~= 'table' then
+	    configureOptimizer(options, unsupData.data:size(2))
+	 else
+	    configureOptimizer(options, #unsupData.data);
+	 end   
       end
-      -- Set of trained layers
-      trainedLayers = {};
-      for l = 1,structure.nLayers do
-	 -- Define the pre-training model
-	 local model = curModel:definePretraining(structure, l, options);
-	 print(tostring(model));
-	 -- Activate CUDA on the model
-	 if options.cuda then model:cuda(); end
-	 -- If classical learning configure the optimizer
-	 if (not options.adaptiveLearning) then 
-	    if torch.type(unsupData.data) ~= 'table' then
-	       configureOptimizer(options, unsupData.data:size(2))
-	    else
-	       configureOptimizer(options, #unsupData.data);
-	    end   
-	 end
-	 epoch = 0;
-	 prevValid = math.huge;
-	 while epoch < options.maxEpochs do
-	    print("Epoch #" .. epoch);
-	    --[[ Adaptive learning ]]--
-	    if options.adaptiveLearning then
-	       -- 1st epochs = Start with purely stochastic (SGD) on single examples
-	       if epoch == 0 then
-		  configureOptimizer({optimization = 'SGD', batchSize = 5,
-				      learningRate = 5e-3},
-		     unsupData.data:size(2));
-	       end
-	       -- Next epochs = Sub-linear approximate algorithm ASGD with mini-batches
-	       if epoch == options.subLinearEpoch then
-		  configureOptimizer({optimization = 'SGD', batchSize = 128,
-				      learningRate = 2e-3},
-		     unsupData.data:size(2));
-	       end
-	       -- Remaining epochs = Advanced learning algorithm user-selected
-	       -- (LBFGS | CG | ADADELTA | ADAGRAD | ADAM | ADAMAX | FISTALS | NAG | RMSPROP | RPROP | CMAES)
-	       if epoch == options.superLinearEpoch then configureOptimizer(options, unsupData.data:size(2)); end
+
+      epoch = 0;
+      prevValid = math.huge;
+
+      while epoch < options.maxEpochs do
+	 print("Epoch #" .. epoch);
+	 --[[ Adaptive learning ]]--
+	 if options.adaptiveLearning then
+	    -- 1st epochs = Start with purely stochastic (SGD) on single examples
+	    if epoch == 0 then
+	       configureOptimizer({optimization = 'SGD', batchSize = 5,
+				   learningRate = 5e-3},
+		  unsupData.data:size(2));
 	    end
-	    --[[ Unsupervised pre-training ]]--
-	    -- Perform unsupervised training of the model
-	    error = curModel:unsupervisedTrain(model, unsupData, options);
-	    print("Reconstruction error (train) : " .. error);
-	    --[[ Validation set checking ]]--
-	    if epoch % options.validationRate == 0 then
-	       -- Check reconstruction error on the validation data
-	       validError = curModel:unsupervisedTest(model, unsupValid,
-						      options);
-	       print("Reconstruction error (valid) : " .. validError);
-	       -- The validation error has risen since last checkpoint
-	       if validError > prevValid then
-		  -- Reload the last saved model
-		  torch.load('results/model-pretrain-layer' .. l .. '.net');
-		  -- Stop the learning
-		  print(" => Stop learning");
-		  break; 
-	       end
-	       -- Otherwise save the current model
-	       torch.save('results/model-pretrain-layer' .. l .. '.net',
-			  model);
-	       -- Keep the current error
-	       prevValid = validError;
+	    -- Next epochs = Sub-linear approximate algorithm ASGD with mini-batches
+	    if epoch == options.subLinearEpoch then
+	       configureOptimizer({optimization = 'SGD', batchSize = 128,
+				   learningRate = 2e-3},
+		  unsupData.data:size(2));
 	    end
-	    epoch = epoch + 1;
-	    -- Collect the garbage
-	    collectgarbage();
+	    -- Remaining epochs = Advanced learning algorithm user-selected
+	    -- (LBFGS | CG | ADADELTA | ADAGRAD | ADAM | ADAMAX | FISTALS | NAG | RMSPROP | RPROP | CMAES)
+	    if epoch == options.superLinearEpoch then configureOptimizer(options, unsupData.data:size(2)); end
 	 end
-	 -- Keep trained layer in table
-	 trainedLayers[l] = model;
-	 -- Retrieve the encoding layer only
-	 model = curModel:retrieveEncodingLayer(model)
-	 -- Put model in evaluation mode
-	 model:evaluate();
-	 -- Prepare a set of activations
-	 forwardedData = {data = {}};
-	 forwardedValid = {data = {}};
-	 -- Perform forward propagation on data
-	 forwardedData.data = model:forward(unsupData.data);
-	 if torch.type(forwardedData.data) ~= 'table' then
-	    forwardedData.data = forwardedData.data:clone()
-	 else
-	    for i = 1,#forwardedData.data do
-	       forwardedData.data[i] = forwardedData.data[i]:clone()
+	 
+	 --[[ Unsupervised pre-training ]]--
+	 -- Perform unsupervised training of the model
+	 error = curModel:unsupervisedTrain(model, unsupData, options);
+	 print("Reconstruction error (train) : " .. error);
+
+	 --[[ Validation set checking ]]--
+	 if epoch % options.validationRate == 0 then
+	    -- Check reconstruction error on the validation data
+	    validError = curModel:unsupervisedTest(model, unsupValid,
+						   options);
+	    print("Reconstruction error (valid) : " .. validError);
+	    -- The validation error has risen since last checkpoint
+	    if validError > prevValid then
+	       -- Reload the last saved model
+	       torch.load('results/model-pretrain-layer' .. l .. '.net');
+	       -- Stop the learning
+	       print(" => Stop learning");
+	       break; 
 	    end
+	    -- Otherwise save the current model
+	    torch.save('results/model-pretrain-layer' .. l .. '.net',
+		       model);
+	    -- Keep the current error
+	    prevValid = validError;
 	 end
-	 -- Replace previous set
-	 unsupData = forwardedData;
-	 -- Perform forward propagation on validation
-	 forwardedValid.data = model:forward(unsupValid.data);
-	 if torch.type(forwardedValid.data) ~= 'table' then
-	    forwardedValid.data = forwardedValid.data:clone()
-	 else
-	    for i = 1,#forwardedValid.data do
-	       forwardedValid.data[i] = forwardedValid.data[i]:clone()
-	    end
-	 end
-	 -- Replace previous set
-	 unsupValid = forwardedValid;
-	 -- Remove garbage
+	 epoch = epoch + 1;
+	 -- Collect the garbage
 	 collectgarbage();
       end
+      
+      -- Keep trained layer in table
+      trainedLayers[l] = model;
+      -- Retrieve the encoding layer only
+      model = curModel:retrieveEncodingLayer(model)
+      -- Put model in evaluation mode
+      model:evaluate();
+      -- Prepare a set of activations
+      forwardedData = {data = {}};
+      forwardedValid = {data = {}};
+
+      -- Perform forward propagation on data
+      forwardedData.data = model:forward(unsupData.data);
+      if torch.type(forwardedData.data) ~= 'table' then
+	 forwardedData.data = forwardedData.data:clone()
+      else
+	 for i = 1,#forwardedData.data do
+	    forwardedData.data[i] = forwardedData.data[i]:clone()
+	 end
+      end
+
+      -- Replace previous set
+      unsupData = forwardedData;
+      -- Perform forward propagation on validation
+      forwardedValid.data = model:forward(unsupValid.data);
+      if torch.type(forwardedValid.data) ~= 'table' then
+	 forwardedValid.data = forwardedValid.data:clone()
+      else
+	 for i = 1,#forwardedValid.data do
+	    forwardedValid.data[i] = forwardedValid.data[i]:clone()
+	 end
+      end
+
+      -- Replace previous set
+      unsupValid = forwardedValid;
+      -- Remove garbage
+      collectgarbage();
    end
 end
