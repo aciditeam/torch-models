5c71afabee4f64804b6ae398bb57dd1b1fff93b1
tbazin <theis.bazin@ens-cachan.fr>
2016-05-17 16:16:54 +0200

    Renamed path to dirData for interface consistency

--- musical_structure_inference/local_parameters.lua
+++ musical_structure_inference/local_parameters.lua
@@ -2,11 +2,13 @@
 
 local M = {}
 
+-- Full Million Song Dataset location
 M.msdb = {}
 
+-- Million Song Dataset 10K-songs-subset location
 M.msdb.subset = {}
 
-M.msdb.subset.path = '/data/Documents/machine_learning/datasets/mir/'..
+M.msdb.subset.dirData = '/data/Documents/machine_learning/datasets/mir/'..
    'MillionSongSubset/data/'
 
 return M
