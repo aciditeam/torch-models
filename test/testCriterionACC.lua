ef1476fa9c8098bf5f7d358d05d5cb0b31e366e8
tbazin <theis.bazin@ens-cachan.fr>
2016-06-23 17:46:19 +0200

    Fixed Accuracy criterion + added testing file

---
+++ test/testCriterionACC.lua
@@ -0,0 +1,54 @@
+----------------------------------------------------------------------
+--
+-- Deep time series learning: Analysis of Torch
+--
+-- Binary accuracy criterion testing function
+--
+----------------------------------------------------------------------
+
+require 'unsup'
+require 'optim'
+require 'torch'
+require 'nninit'
+require 'nn'
+
+require '../criterionAcc.lua'
+
+-- Initialize data
+
+local inputs = torch.rand(4, 12)  -- Random batch of 4 chromas
+print('Input batch of chromagrams:')
+print(inputs)
+
+local offset = 0.1
+local targets = inputs + offset  -- Slightly shifted batch
+print('Target batch of chromagrams (shifted by ' .. -offset .. '):')
+print(targets)
+
+local threshold = 0.2
+print('Threshold is : ' .. threshold)
+
+local inputs_threshold = inputs:gt(threshold)
+local targets_threshold = targets:gt(threshold)
+
+print('Criterion will threshold and compare both batches')
+print('Thresholded inputs:')
+print(inputs_threshold)
+
+print('Thresholded targets:')
+print(targets_threshold)
+
+print('Differences:')
+print(inputs_threshold:ne(targets_threshold))
+
+local criterion = nn.binaryAccCriterion(threshold)
+
+print('Criterion forward:')
+local output = criterion:forward(inputs, targets)
+
+print('Error: ' .. output)
+
+print('Criterion backward:')
+local gradInput = criterion:backward(inputs, targets)
+print('Gradient:')
+print(gradInput)
