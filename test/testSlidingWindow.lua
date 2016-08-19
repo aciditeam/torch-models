fe27b5145f985b34487a122b7f10623d308ce8fe
tbazin <theis.bazin@ens-cachan.fr>
2016-08-19 18:46:06 +0200

    Fixed sliding window test

--- test/testSlidingWindow.lua
+++ test/testSlidingWindow.lua
@@ -1,11 +1,19 @@
 -- Test sliding sliding window module
 
-local slidingWindow = require '../moduleSlidingWindow'
-local getChroma = require './utilities/getChroma'
+local slidingWindow = require '../moduleSlidingWindow.lua'
+local getChroma = require './utils/getChroma'
+require 'rnn'
 
 local slicer = nn.SlidingWindow(1, 64, 64, 1e9, true)
 
+local useSequencer = false
+
 local sequence = getChroma.getChromagram()
+
+if useSequencer then
+   slicer = nn.Sequencer(slicer)
+   sequence = sequence:view(sequence:size(1), 1, sequence:size(2)) 
+end
 
 print(sequence:size())
 
