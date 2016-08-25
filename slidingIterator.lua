c99aabf9d8d806ba50c8d127da90689c65896ada
tbazin <theis.bazin@ens-cachan.fr>
2016-08-25 16:33:30 +0200

    Simplified code using torch.unfold

--- slidingIterator.lua
+++ slidingIterator.lua
@@ -4,133 +4,85 @@ local M = {}
 
 --
 function M.foreach_windowed(f, elems, windowSize, windowStep, no_overlap, verbose)
-   local elems_num = #elems
-   local windowSize = math.min(windowSize, elems_num)
+   local elemsNum = #elems
+   if elemsNum == 0 then return
+      function() return nil end
+   end
+   
+   local windowSize = math.min(windowSize, elemsNum)
    local windowStep = math.max(windowStep or math.floor(windowSize / 10), 1)
-
-   -- Use this for a non-overlapping sliding window (last step then uses
-   -- a smaller window)
-   local no_overlap = no_overlap or false
+   -- Restrict step to window size to not skip any elements
+   windowStep = math.min(windowStep, windowSize)
    
-   -- Avoid skipping some examples because of a step too long
-   if windowStep > windowSize then
-      windowStep = windowSize
+   -- Compute indexes of elements in each window
+   local indexes = torch.range(1, elemsNum)
+   local windowIndexes = indexes:unfold(1, windowSize, windowStep)
+   
+   local nWins = windowIndexes:size(1)
+   
+   -- Use this for a virtually non-overlapping sliding window, i.e.
+   -- make each element appears in only one window, but the window
+   -- start and end pointers still follow given window step and size
+   -- (The last step then uses a smaller window)
+   local skipLength = 0
+   if no_overlap then
+      skipLength = windowSize - windowStep
    end
    
-   local function euclidean_division(a, b)
-      local quotient = math.floor(a / b)
-      local remainder = a % b
+   local function get_window(win_n)
+      local currentIndexes
       
-      return quotient, remainder
-   end
-   
-   local steps_num, remainder_step = euclidean_division(
-      elems_num - windowSize, windowStep)
-   if not(steps_num >= 0) then
-      steps_num = 0
-   end
-   
-   local sizes = {}
-   sizes['winSize'] = windowSize
-   sizes['stepSize'] = windowStep
-   sizes['lastStep'] = remainder_step
-   
-   if verbose then
-      print('Sizes:')
-      print(sizes)
-      print('Main steps number: ' .. steps_num)
-   end
-   
-   -- Current position of window on elements
-   local cur_start = 1
-   local cur_end = sizes['winSize']
-   
-   local function get_window(step_n)
-      if verbose then
-	 print('Step_n: ' .. step_n)
-      end
-
-      if step_n > steps_num + 1 or (step_n > 0 and cur_end >= elems_num) then
-	 return {}, cur_end, cur_end
+      if win_n > nWins then
+	 if windowIndexes[-1][-1] < elemsNum then
+	    -- Make a last window with all elements not yet returned
+	    if no_overlap then
+	       local lastIndex = windowIndexes[-1][-1]
+	       -- Non overlapping last window
+	       currentIndexes = torch.range(lastIndex+1, elemsNum)
+	    else
+	       -- Potentially overlaping last-window
+	       currentIndexes = torch.range(elemsNum - (windowSize-1), elemsNum)
+	    end
+	 else
+	    return {}
+	 end
+      else
+	 -- Normal window
+	 currentIndexes = windowIndexes[win_n]  -- Indexes for current window
       end
       
       local window = {}
       
-      local win_size = sizes['winSize']
-      local step_size = sizes['stepSize']
-      
-      -- get length of next step
-      local next_step
-      local is_last_step = false
-      if step_n <= steps_num then
-	 next_step = step_size  -- a normal step
-      else
-	 if verbose then print('Last step!') end
-	 is_last_step = true
-	 -- in that case, step_n == steps_num, make smaller step
-	 -- with the remaining files
-	 if no_overlap then
-	    next_step = sizes['winSize']
-	    win_size = sizes['lastStep']
-	 else
-	    next_step = sizes['lastStep']
-	 end
+      local content_start = 1
+      if win_n > 1 and win_n <= nWins then
+	 -- Take non-overlapping sliding window in account
+	 content_start = 1 + skipLength
       end
       
-      local win_start = (step_n-1) * step_size + next_step + 1
-      local win_end = win_start + win_size - 1
-      
-      local content_win_start = win_start
-      if is_last_step and no_overlap then win_start = win_end - windowSize + 1 end
-
-      if verbose then
-	 print('Window start: ' .. win_start)
+      for i=content_start, currentIndexes:size(1) do
+	 window[i-content_start + 1] = elems[currentIndexes[i]]
       end
       
-      if no_overlap and step_n > 0 then
-      	 -- Only return new elements within the sliding window
-      	 -- (i.e. end of full window)
-	 if not is_last_step then
-	    content_win_start = win_start + (win_size - next_step)
-	 end
-      end
-      
-      if verbose then
-	 if no_overlap then
-	    print('Window start (trimmed for new content): ' .. content_win_start)
-	 end
-	 print('Window end: ' .. win_end)
-	 print('Remaning elements: ' .. elems_num-win_end)
-      end
-      
-      if verbose and win_end == elems_num then
-	 print('Reached end of input elements')
-      end
-      
-      for elem_n = content_win_start, win_end do
-	 window[1 + elem_n-content_win_start] = elems[elem_n]
-      end
-      
+      local win_start = currentIndexes[1]
+      local win_end = currentIndexes[-1]
       return window, win_start, win_end
    end
-
-   local step_n = 0
+   
+   local win_n = 1
    
    return function()
       collectgarbage(); collectgarbage()
-      if step_n > steps_num+1 then return nil end
+      if win_n > nWins+1 then return nil end
       
       -- get new files to load
-      local window, cur_start, cur_end = get_window(step_n)
+      local window, cur_start, cur_end = get_window(win_n)
       
       -- if window is empty
       if next(window) == nil then return nil end
 
-      -- slice all examples in the batch to have same duration,
-      -- allows putting them all in a single tensor for memory efficiency
       local output = f(window, cur_start, cur_end)
 
-      step_n = step_n + 1
+      win_n = win_n + 1
       
       return output, cur_start, cur_end
    end
